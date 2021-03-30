c93072742ad9b5a8ffa4437f3204e59f6d9b4e12
SophieeV <sophievesin@wanadoo.fr>
2021-03-30 14:11:46 +0200

    sauvegarde 14h10

---
+++ script.js
@@ -0,0 +1,6 @@
+function Result(){
+  let FirstNumber = document.getElementById('FirstNumber').value;
+  let SecondNumber= document.getElementById('SecondNumber').value;
+
+  alert('résulat: ' + FirstNumber * SecondNumber);
+}
